d02e0f2a1114ecbf393258cb80c0719ded0d79b5
Veprev Alexey <Alexey.v.veprev@gmail.com>
2020-04-14 11:53:08 +0300

    sm

--- admin/settings.php
+++ admin/settings.php
@@ -11,9 +11,16 @@ function sm_trending() {
 }
 
 function smt_menu() {
-    add_menu_page("SM Trending", "SM Trending", "manage_options", "sm_trending", "sm_trending", "");
+    add_menu_page("SM Trending", "SM Trending", "sm_manage_options", "sm_trending", "sm_trending", "");
 }
 add_action("admin_menu", "smt_menu");
 
 
-echo get_option('smt_settings');
+$admin = get_role( 'administrator' );
+if ( ! empty( $admin ) ) {
+    $admin->add_cap( 'sm_manage_options' );
+}
+$editor = get_role( 'editor' );
+if ( ! empty( $editor ) ) {
+    $editor->add_cap( 'sm_manage_options' );
+}
